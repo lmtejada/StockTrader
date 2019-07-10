18f67b094230bf2ec7d933f1066c6dbc389ad1fd
lmtejada <laura_mtc@hotmail.com>
2019-07-09 23:12:46 -0500

    Adding functionality for stock trader app

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+    </div>
+</template>
+<script>
+import {mapGetters} from 'vuex';
+import Stock from './Stock.vue';
+
+export default {
+    components: {
+        appStock: Stock
+    },
+    computed: {
+        ...mapGetters({
+            stocks: 'stockPortfolio'
+        })
+    }
+}
+</script>
